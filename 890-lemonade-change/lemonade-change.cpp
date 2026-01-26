2f7e418917be0c0d4b48e78193531ba7fc855d51
aaddyaaa <adyaaryasingh@gmail.com>
2026-01-26 15:52:56 +0530

    Time: 2 ms (13.04%) | Memory: 87.3 MB (10.74%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -5,21 +5,22 @@ public:
         int ten = 0;
         int twenty = 0;
         int n = bills.size();
-        for (int i =0;i<n;i++){
+        for (int i = 0;i<n ; i++){
             if (bills[i]==5){
                 five++;
             }
             else if (bills[i]==10){
-                if(five!=0){
+                if (five){
                 five--;
                 ten++;
                 }
                 else return false;
             }
-            else{
-                if (ten!=0 && five!=0){
-                    ten--;
+            else {
+                if (five && ten){
+                    twenty++;
                     five--;
+                    ten--;
                 }
                 else if (five>=3){
                     five-=3;
