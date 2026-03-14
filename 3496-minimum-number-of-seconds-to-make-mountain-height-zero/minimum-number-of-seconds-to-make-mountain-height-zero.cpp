a227e55fb7dd0ed51035cb5a57ea2159d4865f54
aaddyaaa <adyaaryasingh@gmail.com>
2026-03-14 12:56:35 +0530

    Time: 180 ms (14.29%) | Memory: 34.7 MB (6.83%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-
-    bool canfinish(long long T, int mountainHeight, vector<int>& workerTimes) {
-        long long totalHeight = 0;
-
-        for (int t : workerTimes) {
-
-            long long x = (-1 + sqrt(1 + (8.0 * T) / t)) / 2;
-
-            totalHeight += x;
-
-            if (totalHeight >= mountainHeight)
-                return true;
-        }
-
-        return false;
-    }
-
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
 
-        long long low = 0;
+        priority_queue<
+            pair<long long, pair<long long,long long>>,
+            vector<pair<long long, pair<long long,long long>>>,
+            greater<>
+        > pq;
 
-        long long maxTime = *min_element(workerTimes.begin(), workerTimes.end());
-        long long high = maxTime * 1LL * mountainHeight * (mountainHeight + 1) / 2;
-
-        long long answer = high;
-
-        while (low <= high) {
-
-            long long mid = (low + high) / 2;
-
-            if (canfinish(mid, mountainHeight, workerTimes)) {
-                answer = mid;
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+        for(long long t : workerTimes){
+            pq.push({t, {t, 1}});
         }
 
-        return answer;
+        long long ans = 0;
+
+        for(int i = 0; i < mountainHeight; i++){
+            auto top = pq.top();
+            pq.pop();
+
+            long long time = top.first;
+            long long t = top.second.first;
+            long long k = top.second.second;
+
+            ans = time;
+
+            long long nextTime = time + (k + 1) * t;
+            pq.push({nextTime, {t, k + 1}});
+        }
+
+        return ans;
     }
 };
