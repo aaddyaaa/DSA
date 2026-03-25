182e1ee38ee714862de12196bac7d632c89503d8
aaddyaaa <adyaaryasingh@gmail.com>
2026-03-26 02:13:18 +0530

    Time: 461 ms (60.27%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,5 @@
 select w1.id 
 from weather w1
 join weather w2
-where DATEDIFF(w1.recordDate, w2.recordDate) = 1
-and w1.temperature> w2.temperature 
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature> w2.temperature 
