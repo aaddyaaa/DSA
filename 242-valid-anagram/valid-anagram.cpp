214b67c68731346b1c1b67b62972a4a495cb21aa
aaddyaaa <adyaaryasingh@gmail.com>
2026-04-08 15:20:18 +0530

    Time: 2 ms (50.88%) | Memory: 10.1 MB (10.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if (s==t) return true;
+        unordered_map <char,int> mp;
+        for (char c : s){
+            mp[c]++;
+        }
+        unordered_map <char,int> mpp;
+        for (char c : t){
+            mpp[c]++;
+        }
+        if (mp == mpp) return true;
         else return false;
     }
 };
