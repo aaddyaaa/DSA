095cce12f8e87e2133b87fb0c0dbfa2538dcab7f
aaddyaaa <adyaaryasingh@gmail.com>
2026-05-18 00:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 35.5 MB (71.26%) - LeetSync

---
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+
+        queue<int> q;
+        vector<bool> visited(n, false);
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            // Reached value 0
+            if (arr[i] == 0)
+                return true;
+
+            int forward = i + arr[i];
+            int backward = i - arr[i];
+
+            if (forward < n && !visited[forward]) {
+                visited[forward] = true;
+                q.push(forward);
+            }
+
+            if (backward >= 0 && !visited[backward]) {
+                visited[backward] = true;
+                q.push(backward);
+            }
+        }
+
+        return false;
+    }
+};
