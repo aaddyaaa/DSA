3d40a69e93066f54a10c0601b11eb2d7b5b5e33a
aaddyaaa <adyaaryasingh@gmail.com>
2026-06-02 02:36:11 +0530

    Time: 579 ms (70.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,3 @@
+select DATE_FORMAT(trans_date, '%Y-%m') AS month, country, count(state)as trans_count, SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END)as approved_count, SUM(amount) as trans_total_amount, SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) as approved_total_amount
+from transactions
+group by month, country
